0499b0554a08e076c783f4d2200267c265c5a402
Pavel <isheninp@gmail.com>
2020-10-04 18:33:36 +0300

    Create abcd.rb

---
+++ abcd.rb
@@ -0,0 +1,11 @@
+# Find at least one of any [a,b,c,d] for
+# a**b == c**d AND b**a == d**a
+
+n = [1,2,3,4,5,6,7,8,9,-1,-2,-3,-4,-5,-6,-7,-8,-9]
+
+n.permutation(4).to_a.each do |a|
+  if a[0]**a[1]==a[2]**a[3] && a[1]**a[0]==a[3]**a[2]
+    print a
+    puts
+  end
+end
